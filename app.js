a217e31e77dcf3ecf0439ff7f7eec3b6ff734862
hubatish <zachariah.howell@gmail.com>
2017-12-29 19:24:16 -0800

    needed to use io.in to send msg to all clients..

--- app.js
+++ app.js
@@ -38,12 +38,11 @@ function getPlayerNames() {
 let room = 'default';
 
 io.on('connection', function(client) {
-  client.join(room);
-
   client.on('join', function(data) {
     const id = guid();
     playersForIds.set(id, {});
     console.log('player ids joined' + id + ", " + JSON.stringify(playersForIds));
+    client.join(room);    
     client.emit('clientJoin', {id});
   });
 
@@ -52,7 +51,7 @@ io.on('connection', function(client) {
     console.log('setting name: ' + JSON.stringify(playersForIds));
     playersForIds.get(data.id).name = data.name;
     console.log(playersForIds.get(data.id));
-    client.to(room).emit('allPlayersNames', getPlayerNames());
+    io.in(room).emit('allPlayersNames', getPlayerNames());
   });
 });
 
